7dfe5b0241572c2ae8596de55ddee9f534b9618e
Parrnia <parniaparsa5@gmail.com>
2022-11-16 16:41:38 -0500

    The summation function was created into the program.

--- project.c
+++ project.c
@@ -1,10 +1,19 @@
 #include<stdio.h>
+ int summation (int a , int b){
 
+ int sum=a+b;
+
+ return sum;
+
+ }
 
 int main () {
 	
 	int  a,b;
 scanf("%d%d", &a , &b);
+int s=summation(a,b);
+
+printf("the summation of a and b is %d",s);
 
 	
 	
