a50d1d0725ad739bb91cacb2ecc12e87f255ff60
Parrnia <parniaparsa5@gmail.com>
2022-11-16 16:47:56 -0500

    The  multiplication  function was created into the program.

--- project.c
+++ project.c
@@ -6,6 +6,12 @@
  return sum;
 
  }
+ int zarb (int a, int b){
+
+int s= a*b;
+return s;
+
+}
 
 int main () {
 	
@@ -14,7 +20,9 @@ scanf("%d%d", &a , &b);
 int s=summation(a,b);
 
 printf("the summation of a and b is %d",s);
+int z=zarb(a,b);
 
+printf("\nthe multiplication of a and b is %d",z);
 	
 	
 	
